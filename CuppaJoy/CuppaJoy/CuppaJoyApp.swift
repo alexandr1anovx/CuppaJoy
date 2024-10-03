504100e3c7e629fcbebb1061616d312365315159
andr1anovv <an4lex@gmail.com>
2024-10-04 02:31:57 +0300

    Project added

---
+++ CuppaJoy/CuppaJoy/CuppaJoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuppaJoyApp.swift
+//  CuppaJoy
+//
+//  Created by Alexander Andrianov on 04.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct CuppaJoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
