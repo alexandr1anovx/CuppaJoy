68eff980682479959aea7d34bd414ec4a1d886ac
andr1anovv <an4lex@gmail.com>
2024-10-09 04:09:27 +0300

    Added a reuseful labels for headers

---
+++ CuppaJoy/Views/HeaderLabels.swift
@@ -0,0 +1,32 @@
+//
+//  HeaderLabels.swift
+//  CuppaJoy
+//
+//  Created by Alexander Andrianov on 06.10.2024.
+//
+
+import SwiftUI
+
+struct HeaderLabels: View {
+    let title: String
+    let subtitle: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 24) {
+            Text(title)
+                .font(.poppins(.medium, size: 22))
+                .foregroundStyle(.darkBrown)
+            
+            Text(subtitle)
+                .font(.poppins(.regular, size: 14))
+                .foregroundStyle(.hotGray)
+        }
+    }
+}
+
+#Preview {
+    HeaderLabels(
+        title: "Forgot Password?",
+        subtitle: "Enter your email address"
+    )
+}
