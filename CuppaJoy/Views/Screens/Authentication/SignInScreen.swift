58c2cfef814d2434673a8f4674d342109a1c6212
Alexander Andrianov <an4lex@gmail.com>
2024-11-27 23:01:34 +0200

    UI improvements for the Sign In screen

--- CuppaJoy/Views/Screens/Authentication/SignInScreen.swift
+++ CuppaJoy/Views/Screens/Authentication/SignInScreen.swift
@@ -8,62 +8,53 @@
 import SwiftUI
 
 struct SignInScreen: View {
-    @State private var isShownHome = false
     @State private var phoneNumber = ""
     
     var body: some View {
         NavigationStack {
-            Group {
-                if isShownHome {
-                    CustomTabBar()
-                } else {
-                    ZStack {
-                        Color.primaryBrown.ignoresSafeArea()
-                        
-                        VStack(alignment: .leading, spacing: 30) {
-                            HeaderLabel(
-                                title: "Sign In",
-                                subtitle: "Welcome back!"
-                            )
-                            CustomTextField(
-                                imageName: "phone",
-                                placeholder: "Phone Number",
-                                inputData: $phoneNumber
-                            )
-                            
-                            HStack {
-                                Spacer()
-                                MainButton("Next") {
-                                    withAnimation(.spring) {
-                                        isShownHome = true
-                                    }
-                                }
-                            }
-                            HStack(spacing: 5) {
-                                Text("New member?")
-                                    .font(.poppins(.regular, size: 14))
-                                    .foregroundStyle(.primaryWhite)
-                                    .opacity(0.5)
-                                
-                                NavigationLink {
-                                    SignUpScreen()
-                                } label: {
-                                    Text("Sign Up")
-                                        .font(.poppins(.medium, size: 14))
-                                        .foregroundStyle(.primaryMint)
-                                }
-                            }
+            ZStack {
+                Color.primaryBrown.ignoresSafeArea()
+                
+                VStack(alignment: .leading, spacing: 30) {
+                    HeaderLabel(
+                        title: "Sign In",
+                        subtitle: "Welcome back!"
+                    )
+                    CustomTextField(
+                        imageName: "phone",
+                        placeholder: "Phone Number",
+                        inputData: $phoneNumber
+                    )
+                    
+                    HStack {
+                        Spacer()
+                        MainButton("Next") {
+                            /*
+                             + show the Home screen if credentials are correct
+                             - show an alert with error description
+                             */
                         }
-                        .padding(.horizontal, 30)
                     }
-                }
+                    HStack(spacing: 5) {
+                        Text("New member?")
+                            .font(.poppins(.regular, size: 14))
+                            .foregroundStyle(.primaryWhite)
+                            .opacity(0.5)
+                        
+                        NavigationLink {
+                            SignUpScreen()
+                        } label: {
+                            Text("Sign Up")
+                                .font(.poppins(.medium, size: 14))
+                                .foregroundStyle(.primaryMint)
+                        }
+                    }
+                }.padding(.horizontal, 30)
             }
         }
     }
 }
 
 #if DEBUG
-#Preview {
-    SignInScreen()
-}
+#Preview { SignInScreen() }
 #endif
