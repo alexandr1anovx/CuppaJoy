94cb1ebde81c43af6547c5a778ad6770c0d54a5c
Alexander Andrianov <an4lex@gmail.com>
2024-11-30 23:33:22 +0200

    Improved the UI in Home screen

--- CuppaJoy/Views/Screens/Home/HomeScreen.swift
+++ CuppaJoy/Views/Screens/Home/HomeScreen.swift
@@ -6,21 +6,61 @@
 //
 
 import SwiftUI
-import MapKit
 
 struct HomeScreen: View {
     @Binding var isShownTabBar: Bool
     
     var body: some View {
-        Map {
+        ZStack {
+            Color.cstBrown.ignoresSafeArea()
             
+            VStack(alignment: .leading, spacing: 25) {
+                HomeHeader()
+                CoffeeSelectionView()
+            }
         }
-        
     }
 }
 
 #Preview {
-    HomeScreen(
-        isShownTabBar: .constant(true)
-    )
+    HomeScreen(isShownTabBar: .constant(true))
+}
+
+
+struct HomeHeader: View {
+    var body: some View {
+        HStack(spacing: 20) {
+            VStack(alignment: .leading, spacing: 15) {
+                Text("Alexander")
+                    .font(.poppins(.medium, size: 18))
+                    .foregroundStyle(.cstMint)
+                Text("+380(67)-936-75-89")
+                    .font(.poppins(.regular, size: 14))
+                    .foregroundStyle(.cstWhite)
+                
+                HStack {
+                    Image("mappin")
+                        .resizable()
+                        .frame(width: 23, height: 23)
+                        .foregroundStyle(.cstMint)
+                    Text("Bradford BD1 1PR")
+                        .font(.poppins(.medium, size: 14))
+                        .foregroundStyle(.cstWhite)
+                }
+            }
+            
+            Spacer()
+            
+            Image("bag")
+                .resizable()
+                .frame(width: 28, height: 28)
+                .foregroundStyle(.cstMint)
+            Image("user")
+                .resizable()
+                .frame(width: 28, height: 28)
+                .foregroundStyle(.cstMint)
+        }
+        .padding(.horizontal, 25)
+        .padding(.top)
+    }
 }
