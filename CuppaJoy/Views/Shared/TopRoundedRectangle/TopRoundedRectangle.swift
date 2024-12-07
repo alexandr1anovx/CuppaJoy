5ad3ca9ef30405c99cf64fc2ed34017cbcd5ae5c
Alexander Andrianov <an4lex@gmail.com>
2024-12-07 18:03:12 +0200

    Create a custom rectangle with rounded corners at the top only

---
+++ CuppaJoy/Views/Shared/TopRoundedRectangle/TopRoundedRectangle.swift
@@ -0,0 +1,28 @@
+//
+//  TopRoundedRectangle.swift
+//  CuppaJoy
+//
+//  Created by Alexander Andrianov on 06.12.2024.
+//
+
+import SwiftUI
+
+struct TopRoundedRectangle: Shape {
+  var cornerRadius: CGFloat
+  
+  func path(in rect: CGRect) -> Path {
+    var path = Path()
+    
+    path.move(to: CGPoint(x: 0, y: rect.height))
+    path.addLine(to: CGPoint(x: 0, y: cornerRadius))
+    path.addQuadCurve(to: CGPoint(x: cornerRadius, y: 0), control: CGPoint(x: 0, y: 0))
+    path.addLine(to: CGPoint(x: rect.width - cornerRadius, y: 0))
+    path.addQuadCurve(to: CGPoint(x: rect.width, y: cornerRadius), control: CGPoint(x: rect.width, y: 0))
+    
+    path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+    path.addLine(to: CGPoint(x: 0, y: rect.height))
+    path.closeSubpath()
+    
+    return path
+  }
+}
