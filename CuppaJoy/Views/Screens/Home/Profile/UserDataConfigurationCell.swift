590f6dffbce735a11ade597bbbdde7fbf1c936e2
Alexander Andrianov <an4lex@gmail.com>
2024-12-24 23:32:03 +0200

    The separate "Profile" folder wit the views has been created

---
+++ CuppaJoy/Views/Screens/Home/Profile/UserDataConfigurationCell.swift
@@ -0,0 +1,55 @@
+//
+//  UserDataCell.swift
+//  CuppaJoy
+//
+//  Created by Alexander Andrianov on 21.10.2024.
+//
+
+import SwiftUI
+
+struct UserDataConfigurationCell: View {
+  let image: String
+  let header: String
+  let content: String
+  
+  var body: some View {
+    HStack(spacing: 15) {
+      
+      RoundedRectangle(cornerRadius: 10)
+        .fill(Color.cstBrown)
+        .frame(width: 35, height: 35)
+        .overlay {
+          Image(image)
+            .resizable()
+            .frame(width: 20, height: 20)
+            .foregroundStyle(.cstWhite)
+        }
+      
+      VStack(alignment: .leading, spacing: 5) {
+        Text(header)
+          .font(.poppins(.medium, size: 12))
+          .foregroundStyle(.cstWhite)
+        Text(content)
+          .font(.poppins(.medium, size: 14))
+          .foregroundStyle(.cstCream)
+      }
+      
+      Spacer()
+      Image(systemName: "highlighter")
+        .font(.title3)
+        .foregroundStyle(.cstGray)
+        .onTapGesture {
+          // Modify selected text field
+        }
+    }
+    .padding(.horizontal, 15)
+  }
+}
+
+#Preview {
+  UserDataConfigurationCell(
+    image: "user",
+    header: "Name",
+    content: "Alexander"
+  )
+}
