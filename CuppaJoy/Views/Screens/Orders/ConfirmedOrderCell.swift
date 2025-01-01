840644e4f3ab5630f3cf51f7e318ad638ba18839
Alexander Andrianov <an4lex@gmail.com>
2025-01-02 01:39:26 +0200

    Customized cell design

--- CuppaJoy/Views/Screens/Orders/ConfirmedOrderCell.swift
+++ CuppaJoy/Views/Screens/Orders/ConfirmedOrderCell.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct ConfirmedOrderCell: View {
+  
   var body: some View {
-    Text("Hello, World!")
+    HStack(spacing: 20) {
+      
+      Image(systemName: "cup.and.saucer.fill")
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 50, height: 50)
+        .foregroundStyle(.cstCream)
+      
+      VStack(alignment: .leading, spacing: 5) {
+        Text("Americano")
+          .font(.poppins(.medium, size: 16))
+          .foregroundStyle(.cstWhite)
+        Text("Single, Iced, Medium")
+          .font(.poppins(.regular, size: 12))
+          .foregroundStyle(.cstGray)
+        Text("x 2")
+          .font(.poppins(.bold, size: 14))
+          .foregroundStyle(.cstGray)
+      }
+      
+      Spacer()
+      
+      HStack(spacing: 4) {
+        Text("₴")
+        Text("35.00")
+      }
+      .font(.poppins(.bold, size: 18))
+      .foregroundStyle(.cstCream)
+    }
+    .padding()
+    .background(
+      RoundedRectangle(cornerRadius: 20)
+        .fill(Color.cstBlue)
+    )
+    // Changes the default List row background color
+    .listRowBackground(Color.cstDarkBrown)
   }
 }
 
