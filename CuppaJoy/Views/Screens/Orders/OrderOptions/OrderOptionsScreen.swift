6669f44f30694385b653d125def7becfba7993ab
Alexander Andrianov <an4lex@gmail.com>
2025-01-01 22:08:02 +0200

    Added animation for cup counting. Other minor improvements.

--- CuppaJoy/Views/Screens/Orders/OrderOptions/OrderOptionsScreen.swift
+++ CuppaJoy/Views/Screens/Orders/OrderOptions/OrderOptionsScreen.swift
@@ -11,6 +11,9 @@ struct OrderOptionsScreen: View {
   let selectedCoffee: Coffee
   
   var body: some View {
+    
+    // The views for this screen are located at the bottom of the file.
+    
     ZStack {
       Color.cstDarkBrown.ignoresSafeArea()
       
@@ -24,7 +27,7 @@ struct OrderOptionsScreen: View {
             .foregroundStyle(.cstCream)
           Spacer()
         }
-
+        
         CupCountCell()
         CustomSeparator()
         CupSizeCell()
@@ -33,12 +36,10 @@ struct OrderOptionsScreen: View {
         NavigationLink {
           AssemblerScreen()
         } label: {
-          Image(systemName: "cup.and.heat.waves.fill")
-            .foregroundStyle(.cstCream)
-          HStack(spacing: 5) {
-            Text("Customize with")
-              .font(.poppins(.medium, size: 14))
+          HStack(spacing: 4) {
+            Label("Customize with", systemImage: "cup.and.heat.waves.fill")
               .foregroundStyle(.cstCream)
+              .font(.poppins(.medium, size: 14))
             Text("Assembler")
               .font(.poppins(.bold, size: 15))
               .foregroundStyle(.cstMint)
@@ -50,20 +51,21 @@ struct OrderOptionsScreen: View {
         VStack(spacing: 20) {
           HStack {
             Text("Total Amount:")
-              .font(.poppins(.medium, size: 16))
+              .font(.poppins(.medium, size: 15))
               .foregroundStyle(.cstCream)
             Spacer()
             Text("₴ \(String(format: "%.2f", 50.00))")
-              .font(.poppins(.bold, size: 20))
+              .font(.poppins(.bold, size: 18))
               .foregroundStyle(.cstWhite)
           }
           CustomSeparator()
-          CustomBtn("Make an order") {}
+          CustomBtn("Make an order") {
+            // action
+          }
         }
-         // 20px bottom padding for iPhone SE 3rd.
-         // 0px bottom padding for others iPhones.
-        .padding(.bottom,
-                 UIScreen.current?.bounds.height == 667 ? 20 : 0)
+        // 20px bottom padding for iPhone SE 3rd generation.
+        // 0px bottom padding for others iPhones.
+        .padding(.bottom, UIScreen.current?.bounds.height == 667 ? 20 : 0)
       }
       .padding(.horizontal, 20)
       .navigationBarBackButtonHidden(true)
@@ -76,34 +78,49 @@ struct OrderOptionsScreen: View {
   }
 }
 
-// MARK: View Components
 private extension OrderOptionsScreen {
+  
   struct CupCountCell: View {
-    @State private var count = 0
+    @State private var cupCount: Int = 1
     
     var body: some View {
-      HStack(spacing: 15) {
+      HStack {
         Text("Cup count")
+        
         Spacer()
-        Button("-") { count -= 1 }
-        Text("\(count)")
-        Button("+") { count += 1 }
+        
+        Button("-") {
+          withAnimation { cupCount -= 1 }
+        }
+        .tint(.cstBrown)
+        .disabled(cupCount == 0)
+        .buttonStyle(.bordered)
+        
+        Text("\(cupCount)")
+          .frame(minWidth: 20)
+          .contentTransition(.numericText(value: Double(cupCount)))
+        
+        Button("+") {
+          withAnimation { cupCount += 1 }
+        }
+        .tint(.cstBrown)
+        .disabled(cupCount == 5)
+        .buttonStyle(.bordered)
       }
-      .foregroundStyle(.cstCream)
       .font(.poppins(.medium, size: 15))
-      .buttonStyle(.bordered)
+      .foregroundStyle(.cstCream)
     }
   }
   
   struct CupSizeCell: View {
-    @State private var selectedSize: CoffeeCup = .small
+    @State private var selectedCupSize: CoffeeCup = .small
+    
     var body: some View {
-      
       VStack(alignment: .leading) {
-        Text("Cup size (ml): ")
+        Text("Cup size (ml):")
           .font(.poppins(.medium, size: 15))
           .foregroundStyle(.cstCream)
-        Picker("", selection: $selectedSize) {
+        Picker("", selection: $selectedCupSize) {
           ForEach(CoffeeCup.allCases, id: \.self) { cupSize in
             Text(cupSize.ml)
           }
@@ -115,4 +132,6 @@ private extension OrderOptionsScreen {
   }
 }
 
-#Preview { OrderOptionsScreen(selectedCoffee: .espresso) }
+#Preview {
+  OrderOptionsScreen(selectedCoffee: .espresso)
+}
