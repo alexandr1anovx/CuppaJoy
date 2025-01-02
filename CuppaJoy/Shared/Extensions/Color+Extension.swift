037f86da1c4f14e0ea801f10f877cd4feda42542
Alexander Andrianov <an4lex@gmail.com>
2025-01-02 05:15:17 +0200

    Created an extension for the Color type with a custom gradient.

---
+++ CuppaJoy/Shared/Extensions/Color+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  Color+Extension.swift
+//  CuppaJoy
+//
+//  Created by Alexander Andrianov on 02.01.2025.
+//
+
+import SwiftUI
+
+extension Color {
+  
+  static let mainBackgroundGradient = LinearGradient(
+    colors: [.cstDarkBrown, .cstBlue],
+    startPoint: .topLeading,
+    endPoint: .bottomTrailing
+  )
+}
