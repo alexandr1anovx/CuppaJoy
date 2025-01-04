62b006049b3d29acd1428dc554f1dfa46970f49d
Alexander Andrianov <an4lex@gmail.com>
2025-01-04 03:28:59 +0200

    An unused navigation stack has been deleted

--- CuppaJoy/Views/Screens/Orders/OrderOptions/Assembler/AssemblerScreen.swift
+++ CuppaJoy/Views/Screens/Orders/OrderOptions/Assembler/AssemblerScreen.swift
@@ -11,50 +11,48 @@ struct AssemblerScreen: View {
   @Environment(\.dismiss) var dismiss
   
   var body: some View {
-    NavigationStack {
-      ZStack {
-        Color.cstDarkBrown.ignoresSafeArea()
+    ZStack {
+      Color.mainBackgroundGradient.ignoresSafeArea()
+      
+      VStack {
+        List {
+          CoffeeTypePicker()
+          SortPicker()
+          MilkPicker()
+          SyrupPicker()
+          AdditivePicker()
+          IceCubePicker()
+        }
+        .listStyle(.insetGrouped)
+        .listRowSpacing(20)
+        .scrollContentBackground(.hidden)
         
-        VStack {
-          List {
-            BaristaPicker()
-            CoffeeTypePicker()
-            MilkPicker()
-            SyrupPicker()
-            AdditivePicker()
-            IceCubePicker()
+        VStack(spacing: 15) {
+          HStack {
+            Text("Total Amount:")
+              .font(.poppins(.medium, size: 15))
+              .foregroundStyle(.cstGray)
+            Spacer()
+            Text("₴ \(String(format: "%.2f", 50.0))")
+              .font(.poppins(.bold, size: 18))
+              .foregroundStyle(.cstWhite)
           }
-          .listStyle(.insetGrouped)
-          .listRowSpacing(20)
-          .scrollContentBackground(.hidden)
-          
-          VStack(spacing: 15) {
-            HStack {
-              Text("Total Amount:")
-                .font(.poppins(.medium, size: 16))
-                .foregroundStyle(.cstCream)
-              Spacer()
-              Text("₴ \(String(format: "%.2f", 50.0))")
-                .font(.poppins(.bold, size: 20))
-                .foregroundStyle(.cstWhite)
-            }
-            CustomSeparator()
-            CustomBtn("Confirm") {
-              // get back to order options screen
-              dismiss()
-            }
+          CustomSeparator()
+          CustomBtn("Confirm") {
+            // get back to order options screen
+            dismiss()
           }
-          .padding(.horizontal, 25)
-          .padding(.bottom, UIScreen.current?.bounds.height == 667 ? 20 : 0)
         }
+        .padding(.horizontal, 25)
+        .padding(.bottom, UIScreen.current?.bounds.height == 667 ? 20 : 0)
       }
-      .navigationTitle("Assembler")
-      .navigationBarTitleDisplayMode(.inline)
-      .navigationBarBackButtonHidden(true)
-      .toolbar {
-        ToolbarItem(placement: .topBarLeading) {
-          ArrowBackBtn()
-        }
+    }
+    .navigationTitle("Assembler")
+    .navigationBarTitleDisplayMode(.inline)
+    .navigationBarBackButtonHidden(true)
+    .toolbar {
+      ToolbarItem(placement: .topBarLeading) {
+        ArrowBackBtn()
       }
     }
   }
