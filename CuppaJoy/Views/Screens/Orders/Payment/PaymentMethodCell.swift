e55833e51419afddd7f43e784afb30b15bf731a5
Alexander Andrianov <an4lex@gmail.com>
2025-01-07 17:34:31 +0200

    Added a custom payment cell for the Apple Pay and Credit Card

--- CuppaJoy/Views/Screens/Orders/Payment/PaymentMethodCell.swift
+++ CuppaJoy/Views/Screens/Orders/Payment/PaymentMethodCell.swift
@@ -9,26 +9,53 @@ import SwiftUI
 
 struct PaymentMethodCell: View {
   var body: some View {
-    HStack(spacing: 20) {
-      VStack(alignment: .leading, spacing: 10) {
-        Text("Credit Card")
-          .font(.poppins(.medium, size: 17))
-          .foregroundStyle(.cstWhite)
-        Text("2540 xxxx xxxx 2648")
-          .font(.poppins(.medium, size: 14))
-          .foregroundStyle(.cstGray)
-      }
-      
-      Spacer()
-      
-      Image("visa")
-        .resizable()
-        .scaledToFit()
-        .frame(width: 40, height: 40)
+    List {
+      CreditCardPaymentCell()
+      ApplePaymentCell()
     }
+    .listStyle(.insetGrouped)
+    .listRowSpacing(20)
+    .scrollContentBackground(.hidden)
   }
 }
 
 #Preview {
   PaymentMethodCell()
+}
+
+struct CreditCardPaymentCell: View {
+  var body: some View {
+    HStack(spacing: 20) {
+      VStack(alignment: .leading, spacing: 10) {
+        Text("Credit Card")
+          .font(.poppins(.medium, size: 16))
+          .foregroundStyle(.cstWhite)
+        Text("2540 xxxx xxxx 2648")
+          .font(.poppins(.medium, size: 14))
+          .foregroundStyle(.cstGray)
+      }
+      Spacer()
+      Image("visa")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 40, height: 40)
+    }
+    .listRowBackground(Color.cstBlack)
+  }
+}
+
+struct ApplePaymentCell: View {
+  var body: some View {
+    HStack(spacing: 20) {
+      Text("Apple Pay")
+        .font(.poppins(.medium, size: 16))
+        .foregroundStyle(.cstWhite)
+      Spacer()
+      Image("applePay")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 40, height: 40)
+    }
+    .listRowBackground(Color.cstBlack)
+  }
 }
