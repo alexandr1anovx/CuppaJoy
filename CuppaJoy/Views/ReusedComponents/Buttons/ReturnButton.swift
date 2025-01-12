a78e8a043470fb43850734a9656b16d541a89735
Alexander Andrianov <an4lex@gmail.com>
2025-01-12 16:16:54 +0200

    The view has been renamed and its work logic has been changed.

---
+++ CuppaJoy/Views/ReusedComponents/Buttons/ReturnButton.swift
@@ -0,0 +1,26 @@
+//
+//  DismissXButton.swift
+//  CuppaJoy
+//
+//  Created by Alexander Andrianov on 09.10.2024.
+//
+
+import SwiftUI
+
+struct ReturnButton: View {
+  @Environment(\.dismiss) var dismiss
+  
+  var body: some View {
+    Image(systemName: "arrow.left.circle")
+      .font(.title3)
+      .foregroundStyle(.accent)
+      .opacity(0.8)
+      .onTapGesture {
+        dismiss()
+      }
+  }
+}
+
+#Preview {
+  ReturnButton()
+}
