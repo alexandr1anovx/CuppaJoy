fc67dbedff8f23685dacb9ce797f6cae6dacb310
Alexander Andrianov <an4lex@gmail.com>
2025-01-23 14:39:56 +0200

    Minor changes in appearance and functionality

--- CuppaJoy/Views/Screens/Onboarding/OnboardingScreen.swift
+++ CuppaJoy/Views/Screens/Onboarding/OnboardingScreen.swift
@@ -10,8 +10,7 @@ import SwiftUI
 struct OnboardingScreen: View {
   
   @State private var pageIndex = 0
-  @State private var isAnimatingCircle = false
-  @State private var isAnimatingImage = false
+  @State private var isAnimating = false
   @State private var isOnboardingFinished = false
   
   let pages = OnboardingPage.allCases
@@ -29,12 +28,13 @@ struct OnboardingScreen: View {
   // MARK: - Onboarding Page
   private var onboardingPage: some View {
     ZStack(alignment: .bottomTrailing) {
-      Color.brownDarkBrownGradient.ignoresSafeArea()
-//      LinearGradient(
-//        colors: [.csBrown, .csDarkBrown],
-//        startPoint: .bottomLeading,
-//        endPoint: .topLeading
-//      ).ignoresSafeArea(.all)
+      LinearGradient(
+        colors: isIndexLast ? [.black, .csMint.opacity(0.5)] : [.csBrown,.black],
+        startPoint: .bottom,
+        endPoint: .center
+      )
+      .ignoresSafeArea(.all)
+      .animation(isIndexLast ? .spring(duration: 1) : .none, value: isIndexLast)
       
       VStack(spacing: 20) {
         Spacer()
@@ -54,36 +54,38 @@ struct OnboardingScreen: View {
         .frame(width: 140, height: 140)
         .foregroundStyle(isIndexLast ? .csMint : .csCreamy)
         .shadow(color: isIndexLast ? .csMint : .csCreamy, radius: 3)
-        .scaleEffect(isAnimatingImage ? 0.9 : 1)
-      VStack(alignment: .center, spacing: 10) {
+      VStack(spacing: 10) {
         Text(page.title)
-          .font(.poppins(.bold, size: 17))
+          .font(.title2)
+          .fontWeight(.bold)
+          .fontDesign(.rounded)
           .foregroundStyle(isIndexLast ? .csMint : .csCreamy)
         Text(page.description)
-          .font(.poppins(.medium, size: 13))
-          .foregroundStyle(.gray)
-          .multilineTextAlignment(.center)
+          .font(.footnote)
+          .fontWeight(.medium)
+          .fontDesign(.rounded)
+          .foregroundStyle(isIndexLast ? .white.opacity(0.7) : .gray)
           .lineLimit(2)
       }
+      .multilineTextAlignment(.center)
     }
-    .padding(.horizontal, 40)
-    .animation(.easeInOut, value: pageIndex)
-    .frame(maxWidth: .infinity, minHeight: 250)
+    .padding(.horizontal, 20)
+    .animation(.easeInOut(duration: isIndexLast ? 1 : 0.6), value: pageIndex)
+    .frame(maxWidth: .infinity)
   }
   
   // MARK: - Page Indicator
   private var pageIndicator: some View {
     HStack(spacing: 15) {
       ForEach(0..<pages.count, id: \.self) { index in
-        ZStack {
-          Capsule()
-            .frame(width: 25, height: 2.5)
-            .foregroundStyle(.gray).opacity(0.1)
-          Capsule()
-            .frame(width: pageIndex >= index ? 25 : 0, height: 2.5)
-            .foregroundStyle(isIndexLast ? .csMint : .csCreamy).opacity(0.8)
-            .animation(.spring, value: pageIndex)
-        }
+        Capsule()
+          .frame(width: 20, height: 2)
+          .foregroundStyle(
+            // last page -> all capsules color == .mint
+            // !last page -> current page capsule color == .creamy, others == .gray
+            isIndexLast ? .csMint : (pageIndex >= index ? .csCreamy : .gray.opacity(0.1))
+          )
+          .animation(.easeInOut(duration: 0.5), value: pageIndex)
       }
     }
     .frame(height: 6)
@@ -104,32 +106,28 @@ struct OnboardingScreen: View {
         Group {
           Circle()
             .frame(width: 60, height: 60)
-            .foregroundStyle(isIndexLast ? .csMint : .csCreamy)
-            .scaleEffect(isAnimatingCircle ? 1 : 0, anchor: .leading)
+            .foregroundStyle(isIndexLast ? .csMint.opacity(0.1) : .csCreamy)
+            .scaleEffect(isAnimating ? 1 : 0, anchor: .leading)
           Circle()
             .frame(width: 54, height: 54)
             .foregroundStyle(isIndexLast ? .csMint : .csBrown)
-            .shadow(color: isIndexLast ? .csMint : .csCreamy, radius: 5)
+            .shadow(color: isIndexLast ? .csMint : .csBrown, radius: 3)
         }
         if isIndexLast {
-          Image(.rocket).foregroundStyle(.csBrown)
+          Image(.rocket).foregroundStyle(.black)
         } else {
-          Image(systemName: "arrowshape.right.fill")
-            .foregroundStyle(.csCreamy)
+          Image(systemName: "arrowshape.right.fill").foregroundStyle(.csCreamy)
         }
+      }
+      .onAppear {
+        isAnimating = true
       }
       .onChange(of: pageIndex) { oldValue, newValue in
         // Every time the page index changes,
         // the circle scale animation disappears
         // but it restores after 1.5 seconds.
-        isAnimatingCircle = false
-        withAnimation(.spring) { isAnimatingCircle = true }
-      }
-      .onAppear {
-        isAnimatingCircle = true
-        withAnimation(.easeInOut(duration:2).repeatForever()) {
-          isAnimatingImage = true
-        }
+        isAnimating = false
+        withAnimation(.spring(duration:0.5)) { isAnimating = true }
       }
     }
   }
