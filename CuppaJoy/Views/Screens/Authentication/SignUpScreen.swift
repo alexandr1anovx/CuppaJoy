e1608d45fcb5527cb84c74c1f75d4988a43dd6c2
Alexander Andrianov <an4lex@gmail.com>
2025-02-02 20:17:46 +0200

    Added logic for convenient interaction with text fields. UI improvements

--- CuppaJoy/Views/Screens/Authentication/SignUpScreen.swift
+++ CuppaJoy/Views/Screens/Authentication/SignUpScreen.swift
@@ -7,7 +7,11 @@
 
 import SwiftUI
 
-
+enum AuthFieldContent: Hashable {
+  case initials
+  case phoneNumber
+  case email
+}
 
 enum City: String, CaseIterable {
   case mykolaiv = "Mykolaiv"
@@ -21,9 +25,14 @@ struct SignUpScreen: View {
   @State private var email = ""
   @State private var selectedCity = City.mykolaiv
   @State private var isShownConfirmationSheet = false
+  @FocusState private var fieldContent: AuthFieldContent?
   @Environment(\.dismiss) var dismiss
   
-  // Picker customization settings
+  private var isValidSignUpForm: Bool {
+    !initials.isEmpty && isValidEmail(email)
+  }
+  
+  // MARK: Picker style customizations
   init() {
     UISegmentedControl.appearance().selectedSegmentTintColor = .csBrown
     UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
@@ -36,24 +45,15 @@ struct SignUpScreen: View {
       Color.appBackground.ignoresSafeArea(.all)
       
       ScrollView {
-        VStack(alignment: .center, spacing:0) {
-          AuthHeaderView(
-            title: "Sign Up.",
-            subtitle: "Let's create an account."
-          )
+        VStack(spacing: 0) {
+          AuthHeaderView(title: "Sign Up.", subtitle: "Let's create an account.")
           textFieldList
-          cityPicker.padding(18)
+          cityPicker.padding(20)
           signUpButton.padding(.top,25)
-          signInOption.padding(.top,20)
+          signInOption.padding(.top,25)
           Spacer()
         }
         .padding(.top,20)
-        .sheet(isPresented: $isShownConfirmationSheet) {
-          CodeConfirmationView()
-            .presentationDetents([.large])
-            .presentationCornerRadius(25)
-            .presentationDragIndicator(.visible)
-        }
         .navigationBarBackButtonHidden(true)
         .toolbar {
           ToolbarItem(placement: .topBarLeading) {
@@ -61,44 +61,68 @@ struct SignUpScreen: View {
           }
         }
       }
-      .scrollIndicators(.hidden)
     }
+    .scrollIndicators(.hidden)
+    .onAppear { fieldContent = .initials }
   }
-  // MARK: - Text Field list
+  
+  // MARK: Text Field list
   private var textFieldList: some View {
     List {
-      CustomTextField(
-        imageName: "person",
-        placeholder: "Alexander Pushkin",
+      MyTextField(
+        icon: "person",
+        prompt: "Alexander Pushkin",
         inputData: $initials
       )
-      CustomTextField(
-        imageName: "phone",
-        placeholder: "Enter your phone number",
+      .focused($fieldContent, equals: .initials)
+      .textInputAutocapitalization(.words)
+      .submitLabel(.next)
+      .textInputAutocapitalization(.words)
+      .onSubmit { fieldContent = .phoneNumber }
+      
+      MyTextField(
+        icon: "phone",
+        prompt: "Phone number",
         inputData: $phoneNumber
       )
-      CustomTextField(
-        imageName: "envelope",
-        placeholder: "user@example.com",
+      .focused($fieldContent, equals: .phoneNumber)
+      .submitLabel(.next)
+      .onSubmit { fieldContent = .email }
+      
+      MyTextField(
+        icon: "envelope",
+        prompt: "user@example.com",
         inputData: $email
       )
+      .focused($fieldContent, equals: .email)
+      .keyboardType(.emailAddress)
+      .textInputAutocapitalization(.never)
+      .autocorrectionDisabled(true)
+      .submitLabel(.done)
+      .onSubmit { fieldContent = nil }
     }
     .frame(height: 185)
     .scrollContentBackground(.hidden)
     .scrollIndicators(.hidden)
     .scrollDisabled(true)
   }
-  // MARK: - City Picker
+  
+  // MARK: City Picker
   private var cityPicker: some View {
-    Picker("City", selection: $selectedCity) {
-      ForEach(City.allCases, id: \.self) { city in
-        Text(city.rawValue)
-      }
+    VStack(alignment: .leading) {
+      Text("Select your city:")
+        .font(.poppins(.regular, size: 13))
+        .foregroundStyle(.gray)
+        .padding(.leading, 5)
+      Picker("City", selection: $selectedCity) {
+        ForEach(City.allCases, id: \.self) { city in
+          Text(city.rawValue)
+        }
+      }.pickerStyle(.segmented)
     }
-    .pickerStyle(.segmented)
   }
   
-  // MARK: - Sign Up button
+  // MARK: Sign Up button
   private var signUpButton: some View {
     Button {
       isShownConfirmationSheet.toggle()
@@ -109,12 +133,19 @@ struct SignUpScreen: View {
         .padding(.vertical,8)
         .padding(.horizontal,140)
     }
-    .tint(Color.csBrown)
+    .tint(Color.csDesert.opacity(0.8))
     .buttonStyle(.borderedProminent)
     .shadow(radius: 5)
+    .disabled(!isValidSignUpForm)
+    .sheet(isPresented: $isShownConfirmationSheet) {
+      CodeConfirmationView()
+        .presentationDetents([.large])
+        .presentationCornerRadius(25)
+        .presentationDragIndicator(.visible)
+    }
   }
   
-  // MARK: - Sign In option
+  // MARK: Sign In option
   private var signInOption: some View {
     Button {
       dismiss()
@@ -129,6 +160,15 @@ struct SignUpScreen: View {
       }
       .shadow(radius: 5)
     }
+  }
+  
+  // MARK: Data validation methods
+  // Should be moved to the authentication view model in the future.
+  
+  private func isValidEmail(_ email: String) -> Bool {
+    let emailRegex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,64}$"
+    let emailPredicate = NSPredicate(format: "SELF MATCHES[c] %@", emailRegex)
+    return emailPredicate.evaluate(with: email)
   }
 }
 
