7a3b68ef0a2ecaeae7121a8780f3abb001b20e58
Alexander Andrianov <an4lex@gmail.com>
2025-02-23 23:38:54 +0200

    Updated authentication view model

--- CuppaJoy/ViewModels/AuthenticationViewModel.swift
+++ CuppaJoy/ViewModels/AuthenticationViewModel.swift
@@ -10,6 +10,17 @@ import FirebaseAuth
 import FirebaseFirestore
 import SwiftUI
 
+// MARK: Enums
+
+enum City: String, CaseIterable {
+  case mykolaiv, odesa
+  var title: String { rawValue.capitalized }
+}
+
+enum AuthFieldContent: Hashable {
+  case username, phoneNumber, email
+}
+
 @MainActor
 final class AuthenticationViewModel: ObservableObject {
   
@@ -19,10 +30,28 @@ final class AuthenticationViewModel: ObservableObject {
   @Published var userSession: FirebaseAuth.User?
   private let userCollection = Firestore.firestore().collection("users")
   
+  // MARK: - Form Validation Methods
+  func isValidUsername(_ username: String) -> Bool {
+    let regex = #"^[a-zA-Z-]+ ?.* [a-zA-Z-]+$"#
+    let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+    return predicate.evaluate(with: username)
+  }
   
-  // MARK: Public Methods
+  func isValidPhoneNumber(_ phoneNumber: String) -> Bool {
+    // works only for ukrainian format.
+    let regex = #"^(\+380|0)\d{9}$"#
+    let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+    return predicate.evaluate(with: phoneNumber)
+  }
+  
+  func isValidEmail(_ email: String) -> Bool {
+    let regex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,64}$"
+    let predicate = NSPredicate(format: "SELF MATCHES[c] %@", regex)
+    return predicate.evaluate(with: email)
+  }
+  
+  // MARK: - Entry Methods
   func sendSignInLink() async {
-    
     let actionCodeSettings = ActionCodeSettings()
     actionCodeSettings.url = URL(string: "https://www.example.com")
     // The sign-in operation has to always be completed in the app.
@@ -35,7 +64,6 @@ final class AuthenticationViewModel: ObservableObject {
       print(error.localizedDescription)
       emailLink = email
     }
-    
   }
   
   func signOut() {
@@ -45,5 +73,4 @@ final class AuthenticationViewModel: ObservableObject {
       print("Error signing out: \(signOutError)")
     }
   }
-  
 }
