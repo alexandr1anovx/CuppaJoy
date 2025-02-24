37a81afc42993f29bd1d5c7eeb15803a49998e0a
Alexander Andrianov <an4lex@gmail.com>
2025-02-24 21:06:14 +0200

    Improved logic of custom text field. Added the ability to switch to a secure text field.

--- CuppaJoy/Views/Components/TextField/CSTextField.swift
+++ CuppaJoy/Views/Components/TextField/CSTextField.swift
@@ -7,20 +7,60 @@
 
 import SwiftUI
 
+enum TextFieldContentType {
+  case username
+  case emailAddress
+  case phoneNumber
+  case password
+  
+  var hint: String {
+    switch self {
+    case .username: "Enter your first and last name"
+    case .emailAddress: "Email address"
+    case .phoneNumber: "Phone number"
+    case .password: "Password"
+    }
+  }
+  
+  var icon: String {
+    switch self {
+    case .username: "person.crop.circle"
+    case .emailAddress: "envelope"
+    case .phoneNumber: "phone"
+    case .password: "lock"
+    }
+  }
+}
+
 struct CSTextField: View {
   
-  let icon: String
-  let hint: String
+  let fieldContentType: TextFieldContentType
   @Binding var inputData: String
+  let isSecure: Bool?
+  
+  init(
+    for fieldContentType: TextFieldContentType,
+    inputData: Binding<String>,
+    isSecure: Bool? = nil
+  ) {
+    self.fieldContentType = fieldContentType
+    self.isSecure = isSecure
+    self._inputData = inputData
+  }
   
   var body: some View {
     HStack(spacing: 15) {
-      Image(systemName: icon)
+      Image(systemName: fieldContentType.icon)
         .imageScale(.medium)
         .foregroundStyle(.csCream)
-      TextField(hint, text: $inputData)
-        .font(.system(size: 15))
-        .fontWeight(.medium)
+      
+      if isSecure != nil {
+        SecureField(fieldContentType.hint, text: $inputData)
+      } else {
+        TextField(fieldContentType.hint, text: $inputData)
+          .font(.system(size: 15))
+          .fontWeight(.medium)
+      }
     }
     .listRowInsets(
       EdgeInsets(top: 28, leading: 15, bottom: 22, trailing: 15)
@@ -29,9 +69,5 @@ struct CSTextField: View {
 }
 
 #Preview {
-  CSTextField(
-    icon: "swift",
-    hint: "Enter your password",
-    inputData: .constant("")
-  )
+  CSTextField(for: .password, inputData: .constant(""))
 }
