a02f2a8b8e21a15f47b83dbf08b30c86b56da321
Alexander Andrianov <an4lex@gmail.com>
2025-02-26 16:00:50 +0200

    Removed security option for the custom text

--- CuppaJoy/Views/Components/TextField/CSTextField.swift
+++ CuppaJoy/Views/Components/TextField/CSTextField.swift
@@ -15,7 +15,7 @@ enum TextFieldContentType {
   
   var hint: String {
     switch self {
-    case .username: "Enter your first and last name"
+    case .username: "Name and surname"
     case .emailAddress: "Email address"
     case .phoneNumber: "Phone number"
     case .password: "Password"
@@ -36,15 +36,12 @@ struct CSTextField: View {
   
   let fieldContentType: TextFieldContentType
   @Binding var inputData: String
-  let isSecure: Bool?
   
   init(
     for fieldContentType: TextFieldContentType,
-    inputData: Binding<String>,
-    isSecure: Bool? = nil
+    inputData: Binding<String>
   ) {
     self.fieldContentType = fieldContentType
-    self.isSecure = isSecure
     self._inputData = inputData
   }
   
@@ -53,14 +50,9 @@ struct CSTextField: View {
       Image(systemName: fieldContentType.icon)
         .imageScale(.medium)
         .foregroundStyle(.csCream)
-      
-      if isSecure != nil {
-        SecureField(fieldContentType.hint, text: $inputData)
-      } else {
-        TextField(fieldContentType.hint, text: $inputData)
-          .font(.system(size: 15))
-          .fontWeight(.medium)
-      }
+      TextField(fieldContentType.hint, text: $inputData)
+        .font(.callout)
+        .fontWeight(.medium)
     }
     .listRowInsets(
       EdgeInsets(top: 28, leading: 15, bottom: 22, trailing: 15)
