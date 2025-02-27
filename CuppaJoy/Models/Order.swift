0cae28e7027e86373427d8f5d2098586cc29e06f
Alexander Andrianov <an4lex@gmail.com>
2025-02-27 23:54:02 +0200

    The order model and its types are now signed and conform to the “Codable” protocol. The dynamic price is now calculated on the configurator screen.

--- CuppaJoy/Models/Order.swift
+++ CuppaJoy/Models/Order.swift
@@ -7,24 +7,18 @@
 
 import Foundation
 
-struct Order: Identifiable {
+struct Order: Identifiable, Codable {
   let id: String
-  let coffee: CoffeeType
-  let cupSize: CupSize
+  let coffee: String
+  let cupSize: String
   let cupCount: Int
   let sugarSticks: Int
-  let iceCount: Int
-  let variety: Variety
-  let milk: Milk
-  let flavor: Flavor
+  let iceCubes: Int
+  let variety: String
+  let milk: String
+  let flavor: String
   let timestamp: Date
-  
-  var totalPrice: Double {
-    let basePrice = coffee.price
-    let configurations = cupSize.price + milk.price + flavor.price
-    let total = (basePrice + configurations) * Double(cupCount)
-    return total
-  }
+  var totalPrice: Double
   
   var formattedDate: String {
     let formatter = DateFormatter()
@@ -36,14 +30,23 @@ struct Order: Identifiable {
 struct MockData {
   static let order: Order = Order(
     id: "123",
-    coffee: .americano,
-    cupSize: .medium,
+    coffee: "Americano",
+    cupSize: "Medium",
     cupCount: 1,
     sugarSticks: 2,
-    iceCount: 1,
-    variety: .arabica,
-    milk: .none,
-    flavor: .vanilla,
-    timestamp: Date()
+    iceCubes: 0,
+    variety: "Arabica",
+    milk: "Lactose",
+    flavor: "Caramel",
+    timestamp: .now,
+    totalPrice: 35.20
+  )
+  
+  static let coffee: Coffee = Coffee(
+    id: "10",
+    title: "Americano",
+    description: "Americano's description",
+    rating: 4.5,
+    price: 28.0
   )
 }
