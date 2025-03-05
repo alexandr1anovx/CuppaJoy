c727d98acf53c240c4a2424d36033995c51dce1f
Alexander Andrianov <an4lex@gmail.com>
2025-03-05 23:44:59 +0200

    Added more methods for managing data in the order view model.

--- CuppaJoy/ViewModels/OrderViewModel.swift
+++ CuppaJoy/ViewModels/OrderViewModel.swift
@@ -6,23 +6,142 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
 
+@MainActor
 final class OrderViewModel: ObservableObject {
   
   @Published var ongoingOrders: [Order] = []
   @Published var receivedOrders: [Order] = []
   
-  func addOrder(_ order: Order) {
-    ongoingOrders.append(order)
-  }
+  let userCollection = Firestore.firestore().collection("users")
   
-  func cancelOrder(_ order: Order) {
-    // the item ID creation will be changed in the future.
+  // MARK: Cancel Ongoing Order
+  func cancelOngoingOrder(_ order: Order) {
+    guard let userID = Auth.auth().currentUser?.uid else {
+      print("Cannot get the user data.")
+      return
+    }
+    
     ongoingOrders.removeAll { $0.id == order.id }
+    
+    userCollection.document(userID).collection("ongoingOrders").document(order.id)
+      .delete { error in
+        guard let error = error else {
+          print("Order successfully deleted from Firestore!")
+          return
+        }
+        print("Error deleting order: \(error.localizedDescription)")
+      }
   }
   
-  func retrieveOrders() {
-    // fill the orders array with the user's order history.
+  // MARK: Set Ongoing Orders
+  
+  func setOngoingOrders(_ order: Order) {
+    guard let userID = Auth.auth().currentUser?.uid else {
+      print("Cannot get the user data.")
+      return
+    }
+    let orderData: [String: Any] = [
+      "id": order.id,
+      "coffee": order.coffee,
+      "cupSize": order.cupSize,
+      "cupCount": order.cupCount,
+      "sugarSticks": order.sugarSticks,
+      "iceCubes": order.iceCubes,
+      "variety": order.variety,
+      "milk": order.milk,
+      "flavor": order.flavor,
+      "timestamp": order.timestamp,
+      "points": order.points,
+      "totalPrice": order.totalPrice
+    ]
+    userCollection.document(userID).collection("ongoingOrders").document(order.id)
+      .setData(orderData) { error in
+        if let error = error {
+          print("Error writing document: \(error)")
+        } else {
+          print("Order document successfully written!")
+        }
+      }
   }
   
+  // MARK: Set Received Orders
+  
+  func setReceivedOrders(_ order: Order) {
+    guard let userID = Auth.auth().currentUser?.uid else {
+      print("Cannot get the user data.")
+      return
+    }
+    let orderData: [String: Any] = [
+      "id": order.id,
+      "coffee": order.coffee,
+      "cupSize": order.cupSize,
+      "cupCount": order.cupCount,
+      "sugarSticks": order.sugarSticks,
+      "iceCubes": order.iceCubes,
+      "variety": order.variety,
+      "milk": order.milk,
+      "flavor": order.flavor,
+      "timestamp": order.timestamp,
+      "points": order.points,
+      "totalPrice": order.totalPrice
+    ]
+    userCollection.document(userID).collection("receivedOrders").document(order.id)
+      .setData(orderData) { error in
+        if let error = error {
+          print("Error writing document: \(error)")
+        } else {
+          print("Order document successfully written!")
+        }
+      }
+  }
+  
+  // MARK: Get Ongoing Orders
+  
+  func getOngoingOrders() {
+    guard let userID = Auth.auth().currentUser?.uid else {
+      print("Cannot get the user data.")
+      return
+    }
+    userCollection.document(userID).collection("ongoingOrders")
+      .addSnapshotListener { querySnapshot, error in
+        if let error = error {
+          print("Error listening for documents: \(error)")
+          return
+        }
+        guard let documents = querySnapshot?.documents else {
+          print("Error fetching documents: \(error!)")
+          return
+        }
+        self.ongoingOrders = documents.compactMap { doc -> Order? in
+          try? doc.data(as: Order.self)
+        }
+      }
+  }
+  
+  // MARK: Get Received Orders
+  
+  func getReceivedOrders() {
+    guard let userID = Auth.auth().currentUser?.uid else {
+      print("Cannot get the user data.")
+      return
+    }
+    userCollection.document(userID).collection("receivedOrders")
+      .order(by: "timestamp", descending: true)
+      .addSnapshotListener { querySnapshot, error in
+        if let error = error {
+          print("Error listening for documents: \(error)")
+          return
+        }
+        guard let documents = querySnapshot?.documents else {
+          print("Error fetching documents: \(error!)")
+          return
+        }
+        self.receivedOrders = documents.compactMap { doc -> Order? in
+          try? doc.data(as: Order.self)
+        }
+      }
+  }
 }
