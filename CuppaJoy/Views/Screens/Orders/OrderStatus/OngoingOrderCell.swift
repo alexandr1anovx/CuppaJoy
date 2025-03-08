e7620fcff6bc90c7fcbe83ea39e93d12800b8a2e
Alexander Andrianov <an4lex@gmail.com>
2025-03-08 13:49:16 +0200

    Improved the UI of the ongoing order cell.

--- CuppaJoy/Views/Screens/Orders/OrderStatus/OngoingOrderCell.swift
+++ CuppaJoy/Views/Screens/Orders/OrderStatus/OngoingOrderCell.swift
@@ -10,98 +10,110 @@ import SwiftUI
 struct OngoingOrderCell: View {
   
   let order: Order
-  @State private var isShownDetailedOrderView = false
+  @State private var isShownConfigurationSheet = false
+  @State private var isShownCancelationAlert = false
   @EnvironmentObject var orderViewModel: OrderViewModel
   
   var body: some View {
-    VStack(alignment: .leading, spacing: 12) {
-      header
-      row("Type:", content: order.coffee, icon: .coffeeBeans)
-      row("Size:", content: order.cupSize, icon: .coffee)
-      HStack {
-        orderDetailsButton
+    VStack(alignment: .leading, spacing:12) {
+      // Header
+      HStack{
+        dateLabel
         Spacer()
-        cancelOrderButton
+        priceAndPointsLabel
       }
-      .padding(.top, 5)
+      row("Coffee:", content: order.coffee)
+      row("Size:", content: order.cupSize)
+      // Footer
+      HStack {
+        configurationsButton
+        Spacer()
+        cancelationButton
+      }.padding(.top,8)
+    }
+    .alert("Cancel Order", isPresented: $isShownCancelationAlert) {
+      Button("Cancel", role: .cancel) {}
+      Button("Confirm", role: .destructive) {
+        orderViewModel.cancelOngoingOrder(order)
+      }
+    } message: {
+      Text("Are you sure you want to cancel this order?")
     }
   }
   
-  private var header: some View {
-    HStack {
-      Text(order.formattedDate)
-        .font(.caption)
-        .fontWeight(.medium)
-        .fontDesign(.monospaced)
-        .foregroundStyle(.gray)
-      Spacer()
-      Text("₴\(order.totalPrice, specifier: "%.2f")")
-        .font(.headline)
-        .fontWeight(.bold)
-        .padding(9)
+  private var dateLabel: some View {
+    Text(order.stringDate)
+      .font(.caption)
+      .fontWeight(.semibold)
+      .fontDesign(.monospaced)
+      .foregroundStyle(.gray)
+      .opacity(0.9)
+  }
+  
+  private var priceAndPointsLabel: some View {
+    HStack(spacing:2) {
+      HStack(spacing:6) {
+        Image(.star)
+          .resizable()
+          .frame(width: 15, height: 15)
+        Text(order.stringPoints)
+      }
+      .font(.caption)
+      .foregroundStyle(.orange)
+      .padding(8)
+      .background(.csDarkGrey)
+      .clipShape(.capsule)
+      Text(order.stringPrice)
+        .font(.subheadline)
+        .foregroundStyle(.csCream)
+        .padding(8)
         .background(.csDarkGrey)
         .clipShape(.capsule)
-        .shadow(radius: 5)
     }
-  }
-  
-  private func row(
-    _ title: String,
-    content: String,
-    icon: ImageResource
-  ) -> some View {
-    HStack(spacing: 6) {
-      Image(icon).foregroundStyle(.accent)
-      Text(title).foregroundStyle(.accent)
-      Text(content)
-    }
-    .font(.footnote)
     .fontWeight(.bold)
   }
   
-  private var orderDetailsButton: some View {
+  private func row(_ title: String, content: String) -> some View {
+    HStack(spacing:5) {
+      Text(title).foregroundStyle(.white)
+      Text(content).foregroundStyle(.csCream)
+    }
+    .font(.footnote)
+    .fontWeight(.semibold)
+  }
+  
+  private var configurationsButton: some View {
     Button {
-      isShownDetailedOrderView.toggle()
+      isShownConfigurationSheet.toggle()
     } label: {
-      HStack(spacing: 5) {
-        Image(systemName: "info.circle")
-        Text("Order details")
-      }
-      .font(.caption)
-      .fontWeight(.bold)
-      .foregroundStyle(.white)
-      .padding(10)
-      .background(.csDarkGrey)
-      .clipShape(.capsule)
+      Text("Configurations")
+        .font(.caption)
+        .fontWeight(.bold)
+        .foregroundStyle(.white)
+        .padding(10)
+        .background(.csDarkGrey)
+        .clipShape(.capsule)
     }
     .buttonStyle(.plain)
-    .sheet(isPresented: $isShownDetailedOrderView) {
+    .sheet(isPresented: $isShownConfigurationSheet) {
       DetailedOrderView(order: order)
-        .presentationDetents([.height(250)])
-        .presentationDragIndicator(.hidden)
+        .presentationDetents([.large])
         .presentationCornerRadius(30)
-        .presentationCompactAdaptation(.sheet)
     }
   }
   
-  private var cancelOrderButton: some View {
+  private var cancelationButton: some View {
     Button {
-      withAnimation {
-        orderViewModel.cancelOrder(order)
-      }
+      isShownCancelationAlert.toggle()
     } label: {
-      HStack(spacing: 5) {
-        Image(systemName: "xmark.circle")
-        Text("Cancel order")
-      }
-      .font(.caption)
-      .fontWeight(.bold)
-      .foregroundStyle(.white)
-      .padding(10)
-      .background(.csRed)
-      .clipShape(.capsule)
-    }
-    .buttonStyle(.plain)
+      Text("Cancel Order")
+        .font(.caption)
+        .fontWeight(.bold)
+        .foregroundStyle(.red)
+        .padding(10)
+        .background(.csDarkGrey)
+        .clipShape(.capsule)
+    }.buttonStyle(.plain)
   }
 }
 
