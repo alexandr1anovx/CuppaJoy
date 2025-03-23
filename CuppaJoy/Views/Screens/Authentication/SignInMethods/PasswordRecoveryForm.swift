5ef4650160141a4f02b641db851b4a8acf3ec2e2
Alexander Andrianov <an4lex@gmail.com>
2025-03-23 16:15:55 +0200

    The password recovery form has been improved.

--- CuppaJoy/Views/Screens/Authentication/SignInMethods/PasswordRecoveryForm.swift
+++ CuppaJoy/Views/Screens/Authentication/SignInMethods/PasswordRecoveryForm.swift
@@ -10,12 +10,7 @@ import SwiftUI
 struct PasswordRecoveryForm: View {
   
   @State private var emailAddress = ""
-  @State private var showAlert = false
-  @State private var alertTitle = ""
-  @State private var alertMessage = ""
   @FocusState private var isEmailFocused: Bool
-  
-  @Environment(\.dismiss) var dismiss
   @EnvironmentObject var authViewModel: AuthViewModel
   
   private var isValidEmail: Bool {
@@ -23,72 +18,62 @@ struct PasswordRecoveryForm: View {
   }
   
   var body: some View {
-    VStack(spacing: 20) {
-      Text("Password Recovery")
-        .font(.title2)
-        .fontWeight(.bold)
-        .foregroundStyle(.white)
+    ZStack {
+      Color.csBlack.ignoresSafeArea(edges: .all)
       
-      Text("Enter your email address and we'll send you a link to reset your password.")
-        .font(.footnote)
-        .multilineTextAlignment(.center)
-        .foregroundStyle(.gray)
-        .padding(.horizontal)
-      
-      List {
-        DefaultTextField(for: .emailAddress, inputData: $emailAddress)
-          .focused($isEmailFocused)
-          .keyboardType(.emailAddress)
-          .textInputAutocapitalization(.never)
-          .autocorrectionDisabled(true)
-          .submitLabel(.done)
-          .onSubmit { isEmailFocused = false }
-      }
-      .frame(height: 90)
-      .environment(\.defaultMinListRowHeight, 50)
-      .scrollContentBackground(.hidden)
-      .scrollIndicators(.hidden)
-      .scrollDisabled(true)
-      
-      Button {
-        Task {
-          await sendPasswordResetEmail()
+      VStack(spacing: 20) {
+        Text("Password Recovery")
+          .font(.title2)
+          .fontWeight(.bold)
+          .foregroundStyle(.white)
+        
+        Text("Enter your email address and we'll send you a link to reset your password.")
+          .font(.footnote)
+          .multilineTextAlignment(.center)
+          .foregroundStyle(.gray)
+          .padding(.horizontal)
+        
+        List {
+          DefaultTextField(for: .emailAddress, inputData: $emailAddress)
+            .focused($isEmailFocused)
+            .keyboardType(.emailAddress)
+            .textInputAutocapitalization(.never)
+            .autocorrectionDisabled(true)
+            .submitLabel(.done)
+            .onSubmit { isEmailFocused = false }
         }
-      } label: {
-        ButtonLabel(
-          "Send Recovery Link",
-          textColor: .csCream,
-          pouring: .black
-        )
+        .frame(height: 90)
+        .environment(\.defaultMinListRowHeight, 50)
+        .scrollContentBackground(.hidden)
+        .scrollIndicators(.hidden)
+        .scrollDisabled(true)
+        
+        Button {
+          Task {
+            await authViewModel.sendPasswordResetLink(to: emailAddress)
+          }
+        } label: {
+          ButtonLabel(
+            "Send Recovery Link",
+            textColor: .csCream,
+            pouring: .black
+          )
+        }
+        .disabled(!isValidEmail)
+        .opacity(!isValidEmail ? 0.5 : 1)
+        .alert(item: $authViewModel.alertItem) { alertItem in
+          Alert(
+            title: alertItem.title,
+            message: alertItem.message,
+            dismissButton: alertItem.dismissButton
+          )
+        }
       }
-      .disabled(!isValidEmail)
-      .opacity(!isValidEmail ? 0.5 : 1)
     }
-    .alert(alertTitle, isPresented: $showAlert) {
-      Button("Back to Sign In") {
-        dismiss()
-      }
-    } message: {
-      Text(alertMessage)
-    }
-  }
-  
-  private func sendPasswordResetEmail() async {
-    do {
-      try await authViewModel.resetPassword(for: emailAddress)
-      alertTitle = "Success"
-      alertMessage = "A password reset link has been sent to your email address"
-//      isSuccess = true
-    } catch {
-      alertTitle = "Error"
-      alertMessage = "Failed to send password reset email. Please try again."
-//      isSuccess = false
-    }
-    showAlert = true
   }
 }
 
 #Preview {
   PasswordRecoveryForm()
     .environmentObject(AuthViewModel())
-} 
+}
