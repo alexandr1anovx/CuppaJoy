d51c89103c37c2aedfbd2035d6ad8db4005871d6
Alexander Andrianov <an4lex@gmail.com>
2025-03-23 16:18:38 +0200

    Applied usage of alert manager. Improved code readability.

--- CuppaJoy/ViewModels/AuthViewModel.swift
+++ CuppaJoy/ViewModels/AuthViewModel.swift
@@ -18,10 +18,10 @@ enum City: String, CaseIterable {
 @MainActor
 final class AuthViewModel: ObservableObject {
   
-  // MARK: Properties
   @Published var userSession: FirebaseAuth.User?
   @Published var currentUser: User?
   @Published var selectedCity: City = .mykolaiv
+  @Published var alertItem: AlertItem?
   
   let userCollection = Firestore.firestore().collection("users")
   
@@ -40,24 +40,28 @@ final class AuthViewModel: ObservableObject {
     let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
     return predicate.evaluate(with: fullName)
   }
+  
   func isValidPhoneNumber(_ phoneNumber: String) -> Bool {
     // works only for ukrainian format.
     let regex = #"^(\+380|0)\d{9}$"#
     let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
     return predicate.evaluate(with: phoneNumber)
   }
+  
   func isValidEmail(_ emailAddress: String) -> Bool {
     let regex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,64}$"
     let predicate = NSPredicate(format: "SELF MATCHES[c] %@", regex)
     return predicate.evaluate(with: emailAddress)
   }
+  
   func isValidPassword(_ password: String) -> Bool {
     let regex = #"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"#
     let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
     return predicate.evaluate(with: password)
   }
   
-  // MARK: - Public Methods
+  // MARK: - Public Authentication Methods
+  
   func signIn(with email: String, and password: String) async {
     do {
       let result = try await Auth.auth().signIn(
@@ -67,7 +71,7 @@ final class AuthViewModel: ObservableObject {
       self.userSession = result.user
       await fetchUser()
     } catch {
-      print("User with this data does not exists.")
+      alertItem = AuthAlertContext.failedToSignIn
     }
   }
   
@@ -96,9 +100,30 @@ final class AuthViewModel: ObservableObject {
       try await document.setData(encodedUser)
       await fetchUser()
     } catch {
-      print("This user already exists.")
+      alertItem = AuthAlertContext.failedToSignUp
     }
   }
+  
+  func signOut() {
+    do {
+      try Auth.auth().signOut()
+      self.userSession = nil
+      self.currentUser = nil
+    } catch {
+      alertItem = AuthAlertContext.failedToSignOut
+    }
+  }
+  
+  func sendPasswordResetLink(to email: String) async {
+    do {
+      try await Auth.auth().sendPasswordReset(withEmail: email)
+      alertItem = AuthAlertContext.passwordResetLinkSent
+    } catch {
+      alertItem = AuthAlertContext.passwordResetLinkFailed
+    }
+  }
+  
+  // MARK: - Public Methods
   
   func addCoinsToUser(_ coins: Int) async {
     guard let uid = userSession?.uid else {
@@ -123,25 +148,8 @@ final class AuthViewModel: ObservableObject {
     }
   }
   
-  func signOut() {
-    do {
-      try Auth.auth().signOut()
-    } catch let signOutError as NSError {
-      print("Error signing out: \(signOutError)")
-    }
-  }
+  // MARK: - Private Methods
   
-  func resetPassword(for email: String) async throws {
-    do {
-      try await Auth.auth().sendPasswordReset(withEmail: email)
-      print("Password reset email sent successfully to \(email)")
-    } catch {
-      print("Error sending password reset email: \(error.localizedDescription)")
-      throw error
-    }
-  }
-  
-  // MARK: Private Methods
   private func fetchUser() async {
     guard let uid = userSession?.uid else {
       print("Error when getting user uid")
