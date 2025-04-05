186ce92dbfef2d12fd65b3de83c002e2dd89a2d0
Alexander Andrianov <an4lex@gmail.com>
2025-04-06 01:41:18 +0300

    Created a static view of the profile image with no way to change it.

---
+++ CuppaJoy/Views/Screens/Profile/StaticProfileImageView.swift
@@ -0,0 +1,32 @@
+//
+//  StaticProfileImageView.swift
+//  CuppaJoy
+//
+//  Created by Alexander Andrianov on 05.04.2025.
+//
+
+import SwiftUI
+
+struct StaticProfileImageView: View {
+  @State private var profileImage: UIImage? = nil
+  
+  var body: some View {
+    if let profileImage = profileImage {
+      Image(uiImage: profileImage)
+        .resizable()
+        .scaledToFill()
+        .frame(width: 80, height: 80)
+        .clipShape(.circle)
+    } else {
+      Circle()
+        .fill(Color.black)
+        .frame(width: 80, height: 80)
+        .overlay {
+          Text("No Image")
+            .font(.footnote)
+            .fontWeight(.medium)
+            .foregroundStyle(.white)
+        }
+    }
+  }
+}
