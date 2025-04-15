25c43020b4b0c0596eaab04a15f20ddec3e49970
Alexander Andrianov <an4lex@gmail.com>
2025-04-15 21:30:42 +0300

    Removed extra code. Other minor improvements to the text field.

--- CuppaJoy/Components/Inputs/SecuredInputField.swift
+++ CuppaJoy/Components/Inputs/SecuredInputField.swift
@@ -11,18 +11,14 @@ struct SecuredInputField: View {
   
   @Binding var password: String
   @State private var isShownPassword = false
-  @FocusState private var inputStatus: InputFieldStatus?
-  
-  private var isValidField: Bool {
-    password.count > 6 && password.count <= 20
-  }
+  @FocusState private var inputStatus: InputContentStatus?
   
   var body: some View {
     HStack {
       secureInputField
       Image(systemName: isShownPassword ? "eye" : "eye.slash")
-        .opacity(password.count >= 1 ? 1 : 0)
-        .disabled(password.count < 1)
+        .opacity(!password.isEmpty ? 1:0)
+        .disabled(password.isEmpty)
         .foregroundStyle(.gray)
         .onTapGesture {
           isShownPassword.toggle()
@@ -35,17 +31,15 @@ struct SecuredInputField: View {
       Image(systemName: "lock")
         .frame(width: 18, height: 18)
         .foregroundStyle(.csCream)
-        .opacity(0.8)
       if isShownPassword {
         TextField("Password", text: $password)
-          .font(.subheadline)
-          .keyboardType(.asciiCapable)
           .focused($inputStatus, equals: .notSecured)
+          .keyboardType(.asciiCapable)
+          
       } else {
         SecureField("Password", text: $password)
-          .font(.subheadline)
-          .keyboardType(.asciiCapable)
           .focused($inputStatus, equals: .secured)
+          .keyboardType(.asciiCapable)
       }
     }
     .textInputAutocapitalization(.never)
@@ -62,5 +56,5 @@ struct SecuredInputField: View {
 }
 
 #Preview {
-  SecuredInputField(password: .constant("Password"))
+  SecuredInputField(password: .constant("1"))
 }
