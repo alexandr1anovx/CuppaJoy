0272cc828a39b287ceb150facc7718452336e345
Alexander Andrianov <an4lex@gmail.com>
2025-04-18 21:11:14 +0300

    UI and code style imrovements.

--- CuppaJoy/Features/Profile/ProfileScreen.swift
+++ CuppaJoy/Features/Profile/ProfileScreen.swift
@@ -10,21 +10,23 @@ import CoreImage
 import CoreImage.CIFilterBuiltins
 
 struct ProfileScreen: View {
-  @EnvironmentObject var authViewModel: AuthViewModel
   
+  // MARK: Stored Properties
+  @Binding var path: NavigationPath
+  @Binding var isShownTabBar: Bool
+  @EnvironmentObject var authViewModel: AuthViewModel
   @State private var fullName = ""
   @State private var email = ""
   @State private var accountPassword = ""
   @State private var selectedCity: City = .mykolaiv
   @State private var isShownDeleteAccountAlert = false
   @State private var isShownSavedChangesAlert = false
-  
   @FocusState var inputContent: InputContentType?
-  @Binding var isShownTabBar: Bool
-  @Binding var generalScreenPath: NavigationPath
-  let generator = UINotificationFeedbackGenerator()
   
-  var hasChanges: Bool {
+  let feedbackGenerator = UINotificationFeedbackGenerator()
+
+  // MARK: Computed Properties
+  private var hasChanges: Bool {
     guard let currentUser = authViewModel.currentUser else {
       print("Cannot get current user for checking changes.")
       return false
@@ -36,27 +38,17 @@ struct ProfileScreen: View {
     return changedFullName || changedEmail || changedCity
   }
   
-  var isValidForm: Bool {
+  private var isValidForm: Bool {
     authViewModel.isValid(fullName: fullName) &&
     (email == authViewModel.currentUser?.emailAddress || authViewModel.isValid(email: email))
   }
   
-  struct InputFieldsList: View {
-    @FocusState var inputContent: InputContentType?
-    var body: some View {
-      Text("Hi")
-    }
-  }
-  
+  // MARK: Body
   var body: some View {
     ZStack {
-      Color.csBlack
-        .ignoresSafeArea(.all)
-        .onTapGesture {
-          UIApplication.shared.hideKeyboard()
-        }
+      Color.csBlack.ignoresSafeArea(.all)
       ScrollView {
-        VStack(spacing:0) {
+        VStack {
           EditableProfileImageView()
           personalDataList
           HStack {
@@ -64,10 +56,9 @@ struct ProfileScreen: View {
             Spacer()
             saveChangesButton
           }
-          .padding(.top,20)
           .padding(.horizontal,23)
+          .padding(.top,15)
         }
-        .padding(.top, 10)
       }
     }
     // Alert that appears when the user wants to delete an account.
@@ -77,11 +68,10 @@ struct ProfileScreen: View {
       Button("Delete", role: .destructive) {
         Task {
           try await authViewModel.deleteUser(with: accountPassword)
-          accountPassword = ""
         }
       }
     } message: {
-      Text("Are you sure you want to delete your account? It will delete all your data forever.")
+      Text("Are you sure? It will delete all your data forever.")
     }
     // Alert that appears when the user changes personal data.
     .alert(item: $authViewModel.alertItem) { alertItem in
@@ -99,10 +89,9 @@ struct ProfileScreen: View {
     .navigationBarTitleDisplayMode(.inline)
     .navigationBarBackButtonHidden(true)
     .toolbar {
-      ToolbarItem(placement: .navigationBarLeading) {
+      ToolbarItem(placement: .topBarLeading) {
         Button {
-          isShownTabBar = true
-          generalScreenPath.removeLast()
+          path.removeLast()
         } label: {
           ReturnButtonLabel()
         }
@@ -116,16 +105,9 @@ struct ProfileScreen: View {
         InputField(for: .fullName, data: $fullName)
           .focused($inputContent, equals: .fullName)
           .textInputAutocapitalization(.words)
-          .submitLabel(.next)
-          .textInputAutocapitalization(.words)
-          .onSubmit { inputContent = .email }
-        
-        Button("Edit") {
-          inputContent = .fullName
-        }
-        .font(.callout)
-        .fontWeight(.medium)
-        .foregroundStyle(.gray)
+        Button("Edit") { inputContent = .fullName }
+          .font(.subheadline)
+          .foregroundStyle(.gray)
       }
       .buttonStyle(.plain)
       
@@ -135,23 +117,15 @@ struct ProfileScreen: View {
           .keyboardType(.emailAddress)
           .textInputAutocapitalization(.never)
           .autocorrectionDisabled(true)
-          .submitLabel(.done)
-          .onSubmit { inputContent = nil }
-        
-        Button("Edit") {
-          inputContent = .email
-        }
-        .font(.callout)
-        .fontWeight(.medium)
-        .foregroundStyle(.gray)
+        Button("Edit") { inputContent = .email }
+          .font(.subheadline)
+          .foregroundStyle(.gray)
       }
       .buttonStyle(.plain)
       
-      HStack(spacing: 15) {
-        Image(systemName: "building.2.fill")
-          .frame(width: 18, height: 18)
+      HStack {
+        Image(systemName: "building.2.crop.circle.fill")
           .foregroundStyle(.csCream)
-          .opacity(0.8)
         Picker("City:", selection: $selectedCity) {
           ForEach(City.allCases) { city in
             Text(city.title)
@@ -161,51 +135,45 @@ struct ProfileScreen: View {
         .foregroundStyle(.primary)
       }
     }
-    .listRowSpacing(10)
+    .frame(height: 205)
+    .customListStyle(rowSpacing: 10, shadowRadius: 1)
     .environment(\.defaultMinListRowHeight, 50)
-    .frame(height: 210)
-    .scrollContentBackground(.hidden)
-    .scrollIndicators(.hidden)
     .scrollDisabled(true)
-    .shadow(radius: 1)
   }
   
   private var saveChangesButton: some View {
-    Button {
-      generator.notificationOccurred(.success)
+    Button("Save Changes") {
       Task {
         await authViewModel.updateProfile(
           fullName: fullName,
           email: email,
           city: selectedCity
         )
+        feedbackGenerator.notificationOccurred(.success)
       }
-    } label: {
-      Text("Save Changes")
-        .font(.subheadline)
-        .foregroundStyle(.blue)
     }
+    .font(.subheadline)
+    .foregroundStyle(.blue)
+    .buttonStyle(.bordered)
     .disabled(!hasChanges || !isValidForm)
-    .opacity(!hasChanges || !isValidForm ? 0 : 1)
+    .opacity(!hasChanges || !isValidForm ? 0:1)
   }
   
   private var deleteAccountButton: some View {
     Button {
       isShownDeleteAccountAlert.toggle()
-      generator.notificationOccurred(.error)
+      feedbackGenerator.notificationOccurred(.error)
     } label: {
       Text("Delete Account")
         .font(.footnote)
-        .fontWeight(.medium)
-        .foregroundStyle(.red)
-        .opacity(0.8)
+        .foregroundStyle(.gray)
         .underline()
     }
   }
   
   private func loadUserData() {
     guard let currentUser = authViewModel.currentUser else {
-      print("Cannot retrieve current user data.")
+      print("Cannot retrieve current user data at profile screen.")
       return
     }
     fullName = currentUser.fullName
@@ -215,8 +183,8 @@ struct ProfileScreen: View {
 
 #Preview {
   ProfileScreen(
-    isShownTabBar: .constant(false),
-    generalScreenPath: .constant(NavigationPath())
+    path: .constant(NavigationPath()),
+    isShownTabBar: .constant(false)
   )
   .environmentObject(AuthViewModel.previewMode())
 }
