0a24327c29d61689d8a178722d16d4f646e7cc68
Alexander Andrianov <an4lex@gmail.com>
2025-04-25 18:48:20 +0300

    The coffee config view model has been optimized for the use of the config service.

--- CuppaJoy/Features/Coffee/ViewModels/CoffeeConfigViewModel.swift
+++ CuppaJoy/Features/Coffee/ViewModels/CoffeeConfigViewModel.swift
@@ -6,18 +6,43 @@
 //
 
 import Foundation
-import FirebaseAuth
-import FirebaseFirestore
+import Combine
 
 @MainActor
 final class CoffeeConfigViewModel: ObservableObject {
   
-  @Published var favoriteConfigs: [CoffeeConfig] = []
+  @Published var configs: [CoffeeConfig] = []
+  private let configService = CoffeeConfigService.shared
+  private var cancellables = Set<AnyCancellable>()
+  
+  private func setupBindings() async {
+    do {
+      try await configService.fetchConfigs()
+      Timer.publish(every: 0.5, on: .main, in: .common)
+        .autoconnect()
+        .sink { [weak self] _ in
+          guard let self = self else { return }
+          self.configs = self.configService.configs
+        }
+        .store(in: &cancellables)
+    } catch {
+      print("⚠️ Cannot setup bindings")
+    }
+  }
+  
+  func fetchConfigs() async {
+    do {
+      try await configService.fetchConfigs()
+    } catch {
+      print("⚠️ Failed to fetch user configs: \(error.localizedDescription)")
+    }
+  }
   
   func saveFavoriteConfig(_ config: CoffeeConfig) {
-    favoriteConfigs.append(config)
+    configs.append(config)
   }
+  
   func deleteFavoriteConfig(_ config: CoffeeConfig) {
-    favoriteConfigs.removeAll { $0.id == config.id }
+    configs.removeAll { $0.id == config.id }
   }
 }
