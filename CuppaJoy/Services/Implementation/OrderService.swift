4418bc8cd33eea612aa6ddbe55712511b0a475cb
Alexander Andrianov <an4lex@gmail.com>
2025-05-06 14:32:48 +0300

    Improved readability and removed extra methods.

--- CuppaJoy/Services/Implementation/OrderService.swift
+++ CuppaJoy/Services/Implementation/OrderService.swift
@@ -26,7 +26,6 @@ final class OrderService: OrderServiceProtocol {
   
   var ongoingOrders: [Order] = []
   var receivedOrders: [Order] = []
-  var favoriteConfigs: [CoffeeConfig] = []
   
   func getOngoingOrders() {
     guard let uid = auth.currentUser?.uid else {
@@ -58,64 +57,7 @@ final class OrderService: OrderServiceProtocol {
       }
   }
   
-  func getConfigs() {
-    guard let uid = auth.currentUser?.uid else {
-      print("⚠️Failed to get user ID")
-      return
-    }
-    userCollection.document(uid).collection("configs")
-      .addSnapshotListener { [weak self] snapshot, error in
-        guard let self = self else { return }
-        
-        if let error {
-          print("⚠️ Error listening for documents: \(error)")
-          return
-        }
-        guard let documents = snapshot?.documents else {
-          print("⚠️ Error fetching documents: \(error!)")
-          return
-        }
-        let newConfigs = documents.compactMap { doc -> CoffeeConfig? in
-          try? doc.data(as: CoffeeConfig.self)
-        }
-        DispatchQueue.main.async {
-          self.favoriteConfigs = newConfigs
-        }
-      }
-  }
-  
-  func setFavoriteConfigs(_ config: CoffeeConfig) {
-    guard let uid = auth.currentUser?.uid else {
-      print("⚠️Failed to get user ID")
-      return
-    }
-    
-    let configData: [String: Any] = [
-      "id": config.id,
-      "title": config.title,
-      "cupSize": config.cupSize,
-      "sugarSticks": config.sugarSticks,
-      "iceCubes": config.iceCubes,
-      "variety": config.variety,
-      "milk": config.milk,
-      "flavor": config.flavor
-    ]
-    
-    userCollection.document(uid).collection("configs").document(config.id)
-      .setData(configData) { error in
-        if let error {
-          print("⚠️Failed to save favorite config: \(error)")
-        } else {
-          print("✅Order document successfully updated with the new config!")
-        }
-      }
-  }
-  
-  
-  
-  
-  
-  func setOngoingOrders(_ order: Order) {
+  func setOngoingOrder(_ order: Order) {
     guard let uid = auth.currentUser?.uid else {
       print("⚠️ Failed to get user ID")
       return
@@ -188,36 +130,6 @@ final class OrderService: OrderServiceProtocol {
           self.ongoingOrders.removeAll { order in
             newReceivedOrders.contains(where: { $0.id == order.id })
           }
-        }
-      }
-  }
-  
-  func setReceivedOrders(_ order: Order) {
-    guard let uid = auth.currentUser?.uid else {
-      print("⚠️ Failed to get user ID")
-      return
-    }
-    let orderData: [String: Any] = [
-      "id": order.id,
-      "coffee": order.coffee,
-      "cupSize": order.cupSize,
-      "cupCount": order.cupCount,
-      "sugarSticks": order.sugarSticks,
-      "iceCubes": order.iceCubes,
-      "variety": order.variety,
-      "milk": order.milk,
-      "flavor": order.flavor,
-      "timestamp": order.timestamp,
-      "points": order.points,
-      "totalPrice": order.totalPrice,
-      "status": "received"
-    ]
-    userCollection.document(uid).collection("orders").document(order.id)
-      .setData(orderData) { error in
-        if let error {
-          print("⚠️ Error writing document: \(error)")
-        } else {
-          print("✅ Order document successfully updated with the new received order!")
         }
       }
   }
