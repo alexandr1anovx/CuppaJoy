a84c330ddba739298eec5b9f0b8e1b797f8c9f0e
Alexander Andrianov <an4lex@gmail.com>
2025-05-11 23:20:00 +0300

    Improved code readability.

--- CuppaJoy/Services/Protocols/AuthServiceProtocol.swift
+++ CuppaJoy/Services/Protocols/AuthServiceProtocol.swift
@@ -9,21 +9,11 @@ import Foundation
 import FirebaseAuth
 
 protocol AuthServiceProtocol {
-  var currentUser: FirebaseAuth.User? { get }
-  
-  func signIn(
-    email: String,
-    password: String
-  ) async throws
-  
-  func signUp(
-    email: String,
-    password: String
-  ) async throws -> FirebaseAuth.User
-  
-  func sendEmailVerification() async throws
+  func signIn(email: String, password: String) async throws
+  func signUp(email: String, password: String) async throws -> FirebaseAuth.User
   func signOut() throws
   func deleteUser(withPassword: String) async throws
+  func sendEmailVerification() async throws
   func sendPasswordReset(email: String) async throws
   func fetchUserData(uid: String) async throws -> User
   func saveUserData(for user: User) async throws
