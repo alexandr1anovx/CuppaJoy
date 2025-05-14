33a28b3cc4760e33f5cd8a7edc32f0851c0a5cc4
Alexander Andrianov <an4lex@gmail.com>
2025-05-14 23:20:55 +0300

    Implemented order service methods. Improved readability.

--- CuppaJoy/Features/Orders/ViewModels/OrderViewModel.swift
+++ CuppaJoy/Features/Orders/ViewModels/OrderViewModel.swift
@@ -10,39 +10,56 @@ import Foundation
 @MainActor
 final class OrderViewModel: ObservableObject {
   
-  // MARK: Public Properties
+  // MARK: - Public Properties
   
   @Published var ongoingOrders: [Order] = []
   @Published var receivedOrders: [Order] = []
   
-  // MARK: Private Properties
+  // MARK: - Private Properties
   
   private let orderService: OrderService
   
-  // MARK: - Initializer
+  // MARK: - Init
   
   init(orderService: OrderService = OrderService()) {
     self.orderService = orderService
-    setupBindings()
-    getOngoingOrders()
-    getReceivedOrders()
+    fetchOngoingOrders()
+    fetchReceivedOrders()
   }
   
   // MARK: - Public Methods
   
-  func getOngoingOrders() {
-    orderService.getOngoingOrders()
+  func fetchOngoingOrders() {
+    orderService.getOrders(forStatus: .ongoing) { [weak self] result in
+      DispatchQueue.main.async {
+        switch result {
+        case .success(let orders):
+          self?.ongoingOrders = orders
+        case .failure(let error):
+          print("❌ Failed to get ongoing orders: \(error.localizedDescription)")
+        }
+      }
+    }
   }
   
-  func getReceivedOrders() {
-    orderService.getReceivedOrders()
+  func fetchReceivedOrders() {
+    orderService.getOrders(forStatus: .received) { [weak self] result in
+      DispatchQueue.main.async {
+        switch result {
+        case .success(let orders):
+          self?.receivedOrders = orders
+        case .failure(let error):
+          print("❌ Failed to get ongoing orders: \(error.localizedDescription)")
+        }
+      }
+    }
   }
   
   func setOngoingOrder(_ order: Order) async {
     do {
       try await orderService.setOngoingOrder(order)
     } catch {
-      print("‼️ Cannot set ongoing order: \(error)")
+      print("❌ Failed to set ongoing order: \(error.localizedDescription)")
     }
   }
   
@@ -50,19 +67,18 @@ final class OrderViewModel: ObservableObject {
     do {
      try await orderService.cancelOngoingOrder(order)
     } catch {
-      print("‼️ Cannot cancel ongoing order: \(error)")
+      print("❌ Failed to cancel ongoing order: \(error.localizedDescription)")
     }
   }
-  
-  // MARK: - Private Methods
-  
-  private func setupBindings() {
-    orderService.$ongoingOrders
-      .receive(on: DispatchQueue.main)
-      .assign(to: &$ongoingOrders)
-    
-    orderService.$receivedOrders
-      .receive(on: DispatchQueue.main)
-      .assign(to: &$receivedOrders)
+}
+
+// MARK:  Preview Mode
+
+extension OrderViewModel {
+  static var previewMode: OrderViewModel {
+    let viewModel = OrderViewModel()
+    viewModel.ongoingOrders = [MockData.order]
+    viewModel.receivedOrders = [MockData.order]
+    return viewModel
   }
 }
